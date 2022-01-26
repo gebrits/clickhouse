cf273334ca2e05e05361740e18d31e4988a62671
Geert-Jan Brits <gbrits@gmail.com>
2022-01-26 20:42:47 +0100

    Rewind

--- index.js
+++ index.js
@@ -542,7 +542,7 @@ class QueryCursor {
 			url.searchParams.append('session_id', me.opts.sessionId);
 		}
 		
-		url.searchParams.append('query', query.query);
+		url.searchParams.append('query', query);
 		
 		if (me.connection.isUseGzip) {
 			params.headers['Accept-Encoding']  = 'gzip';
